a9cd0179f92addbada229f19e8bdc98d74deb9d5
Christian Johansen <christian@cjohansen.no>
2010-12-08 21:28:31 +0100

    1.0.0 cleanup, add Makefile

--- pkg/sinon-qunit-1.0.0.js
+++ pkg/sinon-qunit-1.0.0.js
@@ -1,16 +1,16 @@
 /**
- * Sinon.JS 1.0.0, 2010/12/06
+ * sinon-qunit 1.0.0, 2010/12/08
  *
  * @author Christian Johansen (christian@cjohansen.no)
  *
  * (The BSD License)
- *
- * Copyright (c) 2010, Christian Johansen, christian@cjohansen.no
+ * 
+ * Copyright (c) 2010-2011, Christian Johansen, christian@cjohansen.no
  * All rights reserved.
- *
+ * 
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
- *
+ * 
  *     * Redistributions of source code must retain the above copyright notice,
  *       this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above copyright notice,
@@ -19,7 +19,7 @@
  *     * Neither the name of Christian Johansen nor the names of his contributors
  *       may be used to endorse or promote products derived from this software
  *       without specific prior written permission.
- *
+ * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -30,35 +30,33 @@
  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
  */
-/*jslint indent: 2*/
 /*global sinon, QUnit, test*/
 sinon.assert.fail = function (msg) {
-  QUnit.ok(false, msg);
+    QUnit.ok(false, msg);
 };
 
 sinon.assert.pass = function (assertion) {
-  QUnit.ok(true, assertion);
+    QUnit.ok(true, assertion);
 };
 
 sinon.config = {
-  injectIntoThis: true,
-  injectInto: null,
-  properties: ["spy", "stub", "mock", "clock", "sandbox"],
-  useFakeTimers: true,
-  useFakeServer: false
+    injectIntoThis: true,
+    injectInto: null,
+    properties: ["spy", "stub", "mock", "clock", "sandbox"],
+    useFakeTimers: true,
+    useFakeServer: false
 };
 
-(function () {
-  var qTest = QUnit.test;
+(function (global) {
+    var qTest = QUnit.test;
+    
+    QUnit.test = global.test = function (testName, expected, callback, async) {
+        if (arguments.length === 2) {
+            callback = expected;
+            expected = null;
+        }
 
-  QUnit.test = test = function (testName, expected, callback, async) {
-    if (arguments.length === 2) {
-      callback = expected;
-      expected = null;
-    }
-
-    return qTest(testName, expected, sinon.test(callback), async);
-  };
-}());
+        return qTest(testName, expected, sinon.test(callback), async);
+    };
+}(this));
--- test/helper.js
+++ test/helper.js
@@ -1,4 +1,4 @@
-/*jslint indent: 2, onevar: false, eqeqeq: false*/
+/*jslint onevar: false, eqeqeq: false*/
 /*global TestCase*/
 /**
  * @author Christian Johansen (christian@cjohansen.no)
@@ -7,19 +7,19 @@
  * Copyright (c) 2010 Christian Johansen
  */
 function testCase(name, tests) {
-  var jstdCase = {};
-  var jstdTestCase = TestCase; // Silence JsLint...
+    var jstdCase = {};
+    var jstdTestCase = TestCase; // Silence JsLint...
 
-  for (var prop in tests) {
-    if (tests.hasOwnProperty(prop)) {
-      if (!/(^(setUp|tearDown)$)|^test/.test(prop) &&
-          typeof tests[prop] == "function") {
-        jstdCase["test " + prop] = tests[prop];
-      } else {
-        jstdCase[prop] = tests[prop];
-      }
+    for (var prop in tests) {
+        if (tests.hasOwnProperty(prop)) {
+            if (!/(^(setUp|tearDown)$)|^test/.test(prop) &&
+                typeof tests[prop] == "function") {
+                jstdCase["test " + prop] = tests[prop];
+            } else {
+                jstdCase[prop] = tests[prop];
+            }
+        }
     }
-  }
 
-  jstdTestCase(name, jstdCase);
+    jstdTestCase(name, jstdCase);
 }
